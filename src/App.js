faadb69e32e434a18508806399b2642e7fdf7300
multiahn <akb77@nate.com>
2020-11-18 17:50:14 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer'
 
+const customer = {
+  'id':1,
+  'images': 'https://placeimg',
+  'name':'홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}
+
 class App extends Component {
-  return() {
-    <Customer/>
-  };
+  render() {
+    return(
+      <Customer 
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
 }
 
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -3,11 +3,12 @@ import React from 'react'
 class Customer extends React.Component {
     render() {
         return (
-        <div>
-            <h2>홍길동</h2>
-            <p>961222</p>
-            <p>남자</p>
-        </div>
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
         )
     }
 }
